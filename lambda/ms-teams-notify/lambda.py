0711c3015f89f647768346a26f3d93ff583c255e
AlexTLDR <alex.badragan@protonmail.com>
2023-11-09 21:38:04 +0100

    python webhook

---
+++ lambda/ms-teams-notify/lambda.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.8
+import urllib3
+import json
+
+http = urllib3.PoolManager()
+
+def lambda_handler(event, context):
+    # List of webhook URLs
+    webhook_urls = ["https://your-webhook.webhook.office.com", 
+                    "https://your-webhook.webhook.office.com"]
+
+    for url in webhook_urls:
+        msg = {
+            "text": event['Records'][0]['Sns']['Message']
+        }
+        encoded_msg = json.dumps(msg).encode('utf-8')
+        resp = http.request('POST', url, body=encoded_msg)
+        print({
+            "message": event['Records'][0]['Sns']['Message'],
+            "status_code": resp.status,
+            "response": resp.data
+        })
